8ca4e6e3d5dd90838ef0032fba218b0d52c557df
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-03-03 15:39:15 -0300

    responsividade mobile

--- src/components/logoMedtech.jsx
+++ src/components/logoMedtech.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/img/logofinal.png';
 export default function LogoMhedTech(){
     return(
         <div className="logo" style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
-            <img style={{width:"200px"}} src={logo} />
+            <img style={{width:"150px"}} src={logo} />
         </div>
     )
 }
