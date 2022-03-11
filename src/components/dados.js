3e8898a949c8aa092631cb3e09ae97b07e353b5a
JuninhoSouza25 <dnt-bh@hotmail.com>
2022-03-11 08:10:45 -0300

    aniversarios

--- src/components/dados.js
+++ src/components/dados.js
@@ -9,7 +9,7 @@ export const pessoa = [
         cargo: "Analista de Faturamento Júnior",
         setor: "Comercial",
         admissao: "01/09/2015",
-        aniversario: "03/03/1983",
+        aniversario: "03/18/1983",
         foto: "https://live.staticflickr.com/65535/51926931549_e93e77795e_z.jpg",
     },
     {
@@ -22,7 +22,7 @@ export const pessoa = [
         cargo: "Especialista de Produtos",
         setor: "Especialista",
         admissao: "01/10/2020",
-        aniversario: "17/02/1994",
+        aniversario: "02/17/1994",
         foto: "https://live.staticflickr.com/65535/51927227875_97980c045c_z.jpg",
     },
     {
@@ -35,7 +35,7 @@ export const pessoa = [
         cargo: "Consultor de Vendas",
         setor: "Vendas",
         admissao: "23/08/2021",
-        aniversario: "05/07/1985",
+        aniversario: "07/05/1985",
         foto: "https://live.staticflickr.com/65535/51925620547_36ca1208e6_z.jpg",
     },
     {
@@ -48,7 +48,7 @@ export const pessoa = [
         cargo: "Analista Comercial Junior",
         setor: "Comercial",
         admissao: "03/12/2018",
-        aniversario: "18/04/1994",
+        aniversario: "04/18/1994",
         foto: "https://live.staticflickr.com/65535/51925620532_1e45e7aa70_z.jpg",
     },
     {
@@ -61,7 +61,7 @@ export const pessoa = [
         cargo: "Auxiliar de Marketing",
         setor: "Marketing",
         admissao: "01/07/2021",
-        aniversario: "01/04/2009",
+        aniversario: "03/29/1994",
         foto: "https://live.staticflickr.com/65535/51926683478_6f26478e75_z.jpg",
     },
     {
@@ -74,7 +74,7 @@ export const pessoa = [
         cargo: "Gerente de Serviços",
         setor: "Assistência Técnica",
         admissao: "01/04/2009",
-        aniversario: "22/05/1978",
+        aniversario: "05/22/1978",
         foto: "https://live.staticflickr.com/65535/51926683463_6f78334aa6_z.jpg",
     },
     {
@@ -87,7 +87,7 @@ export const pessoa = [
         cargo: "Diretor Técnico",
         setor: "Assistência Técnica",
         admissao: null,
-        aniversario: "23/03/1976",
+        aniversario: "03/23/1976",
         foto: "https://live.staticflickr.com/65535/51926931424_d1700f413f_z.jpg",
     },
     {
@@ -100,7 +100,7 @@ export const pessoa = [
         numero: 31997156077,
         cargo: "Consultor de Vendas",
         admissao: "04/06/2018",
-        aniversario: "04/01/1982",
+        aniversario: "01/04/1982",
         foto: "https://live.staticflickr.com/65535/51926931409_f9ddd9c0c3_z.jpg",
     },
     {
@@ -113,7 +113,7 @@ export const pessoa = [
         cargo: "Auxiliar de Logística",
         setor: "Almoxarifado",
         admissao: "04/02/2022",
-        aniversario: "23/03/1986",
+        aniversario: "03/23/1986",
         foto: "https://live.staticflickr.com/65535/51926931399_60205947aa_z.jpg",
     },
     {
@@ -126,7 +126,7 @@ export const pessoa = [
         cargo: "Auxiliar Comercial",
         setor: "Comercial",
         admissao: "03/08/2021",
-        aniversario: "06/08/1994",
+        aniversario: "08/06/1994",
         foto: "https://live.staticflickr.com/65535/51925620437_4e6b2f36b8_z.jpg",
     },
     {
@@ -139,7 +139,7 @@ export const pessoa = [
         cargo: "Supervisor de Tecnologia",
         setor: "TI",
         admissao: "02/05/2019",
-        aniversario: "25/06/1988",
+        aniversario: "06/25/1988",
         foto: "https://live.staticflickr.com/65535/51926931334_9084d1d89d_z.jpg",
     },
     {
@@ -152,7 +152,7 @@ export const pessoa = [
         cargo: "Técnico de Imagem 2",
         setor: "Assistência Técnica",
         admissao: "13/09/2021",
-        aniversario: "05/01/1979",
+        aniversario: "01/05/1979",
         foto: "https://live.staticflickr.com/65535/51927227680_171db12f30_z.jpg",
     },
     {
@@ -165,7 +165,7 @@ export const pessoa = [
         cargo: "Assistente Financeiro",
         setor: "Financeiro",
         admissao: "03/03/2022",
-        aniversario: "03/09/1999",
+        aniversario: "09/03/1999",
         foto: "https://live.staticflickr.com/65535/51926683288_a06b28edc5_z.jpg",
     },
     {
@@ -178,7 +178,7 @@ export const pessoa = [
         cargo: "Especialista de Produtos",
         setor: "Especialista",
         admissao: "28/10/2019",
-        aniversario: "11/05/1985",
+        aniversario: "05/11/1985",
         foto: "https://live.staticflickr.com/65535/51926591661_8285779e3e_z.jpg",
     },
     {
@@ -191,7 +191,7 @@ export const pessoa = [
         cargo: "Consultor de Vendas",
         setor: "Vendas",
         admissao: "02/01/2014",
-        aniversario: "12/08/1985",
+        aniversario: "08/12/1985",
         foto: "https://live.staticflickr.com/65535/51926683253_74b8f5c8d5_z.jpg",
     },
     {
@@ -204,7 +204,7 @@ export const pessoa = [
         cargo: "Analista Financeiro",
         setor: "Financeiro",
         admissao: "02/01/2020",
-        aniversario: "01/03/1983",
+        aniversario: "03/01/1983",
         foto: "https://live.staticflickr.com/65535/51926591556_4000bc32bc_z.jpg",
     },
     {
@@ -217,7 +217,7 @@ export const pessoa = [
         cargo: "Técnico de Imagem 1",
         setor: "Assistência Técnica",
         admissao: "01/10/2020",
-        aniversario: "18/04/1994",
+        aniversario: "04/18/1994",
         foto: "https://live.staticflickr.com/65535/51926683218_d51c0f4996_z.jpg",
     },
     {
@@ -230,7 +230,7 @@ export const pessoa = [
         cargo: "Recepcionista",
         setor: "Recepção",
         admissao: "02/10/2017",
-        aniversario: "27/03/1985",
+        aniversario: "03/27/1985",
         foto: "https://live.staticflickr.com/65535/51926931074_d8bf3f18db_z.jpg",
     },
     {
@@ -243,7 +243,7 @@ export const pessoa = [
         cargo: "Técnico Júnior 2",
         setor: "Assistência Técnica",
         admissao: "20/05/2019",
-        aniversario: "17/02/1989",
+        aniversario: "02/17/1989",
         foto: "https://live.staticflickr.com/65535/51926931084_67d22a24dc_z.jpg",
     },
     {
@@ -256,7 +256,7 @@ export const pessoa = [
         cargo: "Auxiliar Administrativo 2",
         setor: "Assistência Técnica",
         admissao: "02/03/2020",
-        aniversario: "13/06/1996",
+        aniversario: "06/13/1996",
         foto: "https://live.staticflickr.com/65535/51926683098_c84384c689_z.jpg",
     },
     {
@@ -269,7 +269,7 @@ export const pessoa = [
         cargo: "Auxiliar de Estoque",
         setor: "Almoxarifado",
         admissao: "02/08/2021",
-        aniversario: "13/09/1986",
+        aniversario: "09/13/1986",
         foto: "https://live.staticflickr.com/65535/51926591361_d4bb239b33_z.jpg",
     },
     {
@@ -282,7 +282,7 @@ export const pessoa = [
         cargo: "Assistente Financeiro",
         setor: "Financeiro",
         admissao: "01/03/2021",
-        aniversario: "08/12/2000",
+        aniversario: "12/08/2000",
         foto: "https://live.staticflickr.com/65535/51926683048_c1615cddda_z.jpg",
     },
     {
@@ -295,7 +295,7 @@ export const pessoa = [
         cargo: "Consultora de Vendas",
         setor: "Vendas",
         admissao: "10/01/2022",
-        aniversario: "04/08/1958",
+        aniversario: "08/04/1958",
         foto: "https://live.staticflickr.com/65535/51926683033_24de37e135_z.jpg",
     },
     {
@@ -308,7 +308,7 @@ export const pessoa = [
         cargo: "Auxiliar de Almoxarifado",
         setor: "Almoxarifado",
         admissao: "26/04/2021",
-        aniversario: "31/08/1996",
+        aniversario: "08/31/1996",
         foto: "https://live.staticflickr.com/65535/51926683018_74a0df881d_z.jpg",
     },
     {
@@ -321,7 +321,7 @@ export const pessoa = [
         cargo: "Analista de RH",
         setor: "Recursos Humanos",
         admissao: "03/03/2022",
-        aniversario: "26/03/1988",
+        aniversario: "03/26/1988",
         foto: "https://live.staticflickr.com/65535/51926682983_511b9b8177_z.jpg",
     },
     {
@@ -334,7 +334,7 @@ export const pessoa = [
         cargo: "Consultor de Vendas",
         setor: "Vendas",
         admissao: "10/01/2022",
-        aniversario: "09/06/1984",
+        aniversario: "06/09/1984",
         foto: "https://live.staticflickr.com/65535/51926930924_4e84dff23c_z.jpg",
     },
     {
@@ -347,7 +347,7 @@ export const pessoa = [
         cargo: "Coordenador de Estoque",
         setor: "Almoxarifado",
         admissao: "02/04/2020",
-        aniversario: "30/06/1995",
+        aniversario: "06/30/1995",
         foto: "https://live.staticflickr.com/65535/51926931874_3645d80b1b_z.jpg",
     },
     {
@@ -360,7 +360,7 @@ export const pessoa = [
         cargo: "Gerente de Vendas",
         setor: "Vendas",
         admissao: "01/10/2020",
-        aniversario: "26/11/1991",
+        aniversario: "11/26/1991",
         foto: "https://live.staticflickr.com/65535/51926592221_ff1a90eb0b_z.jpg",
     },
     {
@@ -373,7 +373,7 @@ export const pessoa = [
         cargo: "Consultor de Vendas",
         setor: "Vendas",
         admissao: "25/10/2021",
-        aniversario: "19/11/1984",
+        aniversario: "11/19/1984",
         foto: "https://live.staticflickr.com/65535/51926592216_81fc987182_z.jpg",
     },
     {
@@ -386,7 +386,7 @@ export const pessoa = [
         cargo: "Especialista de Produtos Junior",
         setor: "Comercial",
         admissao: "31/08/2020",
-        aniversario: "09/03/1986",
+        aniversario: "03/09/1986",
         foto: "https://live.staticflickr.com/65535/51926683858_76d2d836f3_z.jpg",
     },
     {
@@ -399,7 +399,7 @@ export const pessoa = [
         cargo: "Gerente de Vendas Imagem",
         setor: "Vendas",
         admissao: "15/09/2021",
-        aniversario: "26/03/1970",
+        aniversario: "03/26/1970",
         foto: "https://live.staticflickr.com/65535/51927228165_d73f473de6_z.jpg",
     },
     {
@@ -412,7 +412,7 @@ export const pessoa = [
         cargo: "Responsável Técnica | Farmaceutica",
         setor: "Qualidade",
         admissao: "07/02/2022",
-        aniversario: "04/05/1982",
+        aniversario: "05/04/1982",
         foto: "https://live.staticflickr.com/65535/51926592161_fd4d240c34_z.jpg",
     },
     {
@@ -425,7 +425,7 @@ export const pessoa = [
         cargo: "CEO",
         setor: "Diretoria",
         admissao: null,
-        aniversario: "16/03/1956",
+        aniversario: "03/16/1956",
         foto: "https://live.staticflickr.com/65535/51926592151_0d70d21102_z.jpg",
     },
     {
@@ -438,7 +438,7 @@ export const pessoa = [
         cargo: "Gerente Comercial",
         setor: "Comercial",
         admissao: "05/11/2014",
-        aniversario: "07/08/1983",
+        aniversario: "08/07/1983",
         foto: "https://live.staticflickr.com/65535/51926592131_76601d5a7d_z.jpg",
     },
     {
@@ -451,7 +451,7 @@ export const pessoa = [
         cargo: "Auxiliar de TI",
         setor: "TI",
         admissao: "14/02/2022",
-        aniversario: "28/11/2001",
+        aniversario: "11/28/2001",
         foto: "https://live.staticflickr.com/65535/51926683783_bf851bf55c_z.jpg",
     },
     {
@@ -464,7 +464,7 @@ export const pessoa = [
         cargo: "Analista de Licitação",
         setor: "Comercial",
         admissao: "08/11/2021",
-        aniversario: "05/02/1988",
+        aniversario: "02/05/1988",
         foto: "https://live.staticflickr.com/65535/51927228115_ce9323a98f_z.jpg",
     },
     {
@@ -477,7 +477,7 @@ export const pessoa = [
         cargo: "Especialista Fiscal",
         setor: "Comercial",
         admissao: "23/11/2021",
-        aniversario: "25/01/1991",
+        aniversario: "01/25/1991",
         foto: "https://live.staticflickr.com/65535/51927228105_31836f50b1_z.jpg",
     },
     {
@@ -490,7 +490,7 @@ export const pessoa = [
         cargo: "Diretor de Vendas",
         setor: "Diretoria",
         admissao: null,
-        aniversario: "28/04/1982",
+        aniversario: "04/28/1982",
         foto: "https://live.staticflickr.com/65535/51926683738_2c3be141bd_z.jpg",
     },
     {
@@ -503,7 +503,7 @@ export const pessoa = [
         cargo: "Especialista de Produtos",
         setor: "Especialista",
         admissao: "11/02/2019",
-        aniversario: "10/08/1989",
+        aniversario: "08/10/1989",
         foto: "https://live.staticflickr.com/65535/51926592031_ae12c4f3c0_z.jpg",
     },
     {
@@ -516,7 +516,7 @@ export const pessoa = [
         cargo: "Consultor de Vendas",
         setor: "Vendas",
         admissao: "03/02/2020",
-        aniversario: "18/01/1976",
+        aniversario: "01/18/1976",
         foto: "https://live.staticflickr.com/65535/51925620697_1ec7b92f09_z.jpg",
     },
     {
@@ -529,7 +529,7 @@ export const pessoa = [
         cargo: "Técnico Junior 2",
         setor: "Assistência Técnica",
         admissao: "31/01/2022",
-        aniversario: "05/08/1986",
+        aniversario: "08/05/1986",
         foto: "https://live.staticflickr.com/65535/51926931664_c33c7cdcae_z.jpg",
     },
     {
@@ -542,7 +542,7 @@ export const pessoa = [
         cargo: "Diretor de Vendas",
         setor: "Vendas",
         admissao: "12/11/2012",
-        aniversario: "03/11/1981",
+        aniversario: "11/03/1981",
         foto: "https://live.staticflickr.com/65535/51927359672_3ecaf94151_z.jpg",
     },
     {
@@ -555,7 +555,7 @@ export const pessoa = [
         cargo: "Consultor de Vendas",
         setor: "Vendas",
         admissao: "14/10/2021",
-        aniversario: "19/10/1976",
+        aniversario: "10/19/1976",
         foto: "https://live.staticflickr.com/65535/51926683653_057dd81e62_z.jpg",
     },
     {
@@ -568,7 +568,7 @@ export const pessoa = [
         cargo: "Supervisora Comercial",
         setor: "Comercial",
         admissao: "08/09/2017",
-        aniversario: "30/04/1979",
+        aniversario: "04/30/1979",
         foto: "https://live.staticflickr.com/65535/51926683628_e54f41cfbd_z.jpg",
     },
     {
@@ -581,7 +581,7 @@ export const pessoa = [
         cargo: "Diretora de Operações",
         setor: "Diretoria | Comercial",
         admissao: null,
-        aniversario: "06/10/1986",
+        aniversario: "10/06/1986",
         foto: "https://live.staticflickr.com/65535/51925620652_ca8ecc181c_z.jpg",
     },
     {
@@ -594,7 +594,7 @@ export const pessoa = [
         cargo: "Consultora de Vendas",
         setor: "Vendas",
         admissao: "05/10/2020",
-        aniversario: "09/12/1994",
+        aniversario: "12/09/1994",
         foto: "https://live.staticflickr.com/65535/51926591931_cbb12b57d2_z.jpg",
     },
     {
@@ -607,7 +607,7 @@ export const pessoa = [
         cargo: "Técnico Junior 2",
         setor: "Assistência Técnica",
         admissao: "01/08/2009",
-        aniversario: "19/11/1977",
+        aniversario: "11/19/1977",
         foto: "https://live.staticflickr.com/65535/51926931594_169e363b48_z.jpg",
     },
     {
@@ -620,7 +620,7 @@ export const pessoa = [
         cargo: "Consultor de Vendas",
         setor: "Vendas",
         admissao: "05/10/2020",
-        aniversario: "26/12/1962",
+        aniversario: "12/26/1962",
         foto: "https://live.staticflickr.com/65535/51926683583_2001aca7ed_z.jpg",
     },
 ]
